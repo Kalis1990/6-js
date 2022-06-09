232da7656848363ad3bc2ae52430657bc01d3925
Rafal K <Rafaledmund@yahoo.com>
2022-06-09 10:41:31 +0300

    nested ifs, and logical operators

--- js/if.js
+++ js/if.js
@@ -11,6 +11,10 @@ Palyginimo operatoriai:
 visi >, <, >=, <=, ==, !=, ===, !==
 nauduotini: >, <, >=, <=, ===, !==
 nenauduotini: ==, !=
+
+Loginiai operatoriai:
+&& (and) - turi tenkiti visos salygos
+||(or) - turi tenkinti bent viena salyga
 */
 
 const a = 77;
@@ -41,3 +45,78 @@ if(x === 5){
 } else{
     console.log('ne');
 }
+
+// const diena = 1;
+// if (diena === 1) {
+//     console.log('pirmadienis');
+// } else{
+//     console.log('ne pirmadienis');
+// }
+
+const diena = 4;
+if (diena === 1) {
+    console.log('pirmadienis');
+} else{
+    if (diena === 2){
+        console.log('antradienis');
+    } else{
+        if (diena === 3){
+            console.log('treciadienis');
+        } else{
+            if (diena === 4){
+                console.log('ketvirtadienis');
+            } else{
+                if (diena === 5){
+                    console.log('penktadienis');
+                } else{
+                    console.log('tokios dienos nera')
+                }
+            }
+        }
+    }
+}
+
+const day = 7;
+if (day === 1){
+    console.log('pirmadienis');
+} else if (day === 2){
+    console.log('antradienis')
+} else if (day === 3){
+    console.log('treciadienis')
+} else if (day === 4){
+    console.log('ketvirtadienis')
+} else if (day === 5){
+    console.log('penktadienis')
+} else if (day === 6){
+    console.log('sestadienis');
+} else if (day === 7){
+    console.log('sekmadienis');
+} else{
+    console.log('tokios dienos nera');
+}
+console.log('----------------------------------------');
+
+const temp = 18;
+const lietus = true;
+const nuoKadaSilta = 12;
+
+if(lietus === true){
+    console.log('1)pasiimk skieti')
+}else{
+    if(temp > nuoKadaSilta){
+    console.log('1)gali eiti su maike');
+    }else{
+        console.log('1)pasiimk striuke');
+    }
+}
+
+if(lietus === true && temp >= nuoKadaSilta){
+    console.log('2)varom nuogi')
+} else if (lietus === true && temp < nuoKadaSilta){
+    console.log('2)pasiimk palta ir skieti');
+} else if (lietus === false && temp >= nuoKadaSilta){
+    console.log('2)gali eiti su maike');
+} else if (lietus === false && temp < nuoKadaSilta){
+            console.log('2)pasiimk striuke');
+}
+
---
+++ js/nd.js
@@ -0,0 +1,66 @@
+//Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
+//Po kiekvieno jų inicijavimo, išvesti į console
+const a = 5;
+const b = 10;
+const c = 15;
+console.log(a);
+console.log(b);
+console.log(c);
+
+//Sukurti 3 kintamuosius su teksto tipo reikšmėmis
+//Po kiekvieno jų inicijavimo, išvesti į console
+const n1 = 'Petriukas';
+const n2 = 'Elvis';
+const n3 = 'Zuokas';
+console.log(n1);
+console.log(n2);
+console.log(n3);
+
+//Sukurti 3 sąrašo tipo kintamuosius su penkiomis skaičių tipo reikšmėmis
+//Po kiekvieno jų inicijavimo, išvesti į console
+let d = [1, 2, 3, 4, 5];
+let e = [2, 3, 4, 5, 6];
+let f = [3, 4, 5, 6, 7];
+console.log(d);
+console.log(e);
+console.log(f);
+
+//Sukurti 3 sąrašo tipo kintamuosius su penkiomis teksto tipo reikšmėmis
+//Po kiekvieno jų inicijavimo, išvesti į console
+const person = {
+    firstName : "John",
+    lastName  : "Doe",
+    age       : '50',
+    eyeColor  : "blue",
+    bodySize  : 'slim'
+  };
+const person2 = {
+    firstName : "Rambo",
+    lastName  : "Killer",
+    age       : '30',
+    eyeColor  : "Dark",
+    bodySize  : 'Fit'
+};
+const Person3 =  {
+    firstName : "Rambo",
+    lastName  : "Killer",
+    age       : '30',
+    eyeColor  : "Dark",
+    bodySize  : 'Fit'
+};
+console.log(person);
+console.log(person2);
+console.log(Person3);
+//Susumuoti visus skaičiaus tipo kintamuosius
+//Rezultatą išvesti į console
+console.log('Suma visu kintamuju yra', a+b+c+d+e+f);
+
+//Sujungti visus teksto tipo kintamuosius taip, jog tarp jų būtų sudarytas tarpas
+//Rezultatą išvesti į console
+//colsole.log(person.firstName + " is " + person.age + " years old.");
+
+//Apskaičiuoti vertę iš sąrašų kurių verčių tipas yra skaičiai, pagal pateiktą logiką
+//1-2+3-4+5
+//Rezultatą išvesti į console
+
+
