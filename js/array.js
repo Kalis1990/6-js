0dee740517c995096946e0172a93a6633ed11272
Rafal K <Rafaledmund@yahoo.com>
2022-06-09 11:54:49 +0300

    masyvai

---
+++ js/array.js
@@ -0,0 +1,27 @@
+/*
+ARRAY (kompleksinis duomenu tipas)
+ltu: masyvas, sarasa, matrica (daugiamatis masyvas)
+
+*/
+
+// const p1 = 10;
+// const p2 = 2;
+// const p3 = 4;
+// const p4 = 6;
+// const p5 = 8;
+
+// const vidurkis = (p1 + p2 + p3 + p4 + p5) / 5;
+// console.log(vidurkis)c
+const students = ['petras', 'maryte', 'voras', 'jurgis', 'techas'];
+/*             0   1  2  3  4 */
+const marks = [10, 2, 8, 4, 6];
+
+// const suma = marks[0] + marks[1] + marks[2] + marks[3] + marks[4];
+const suma = marks[0] + marks[1] + marks[2] + marks[3] + marks[4];
+// const kiekis = 5;
+const kiekis = marks.length;
+const vidurkis = suma / kiekis;
+console.log('vidurkis', vidurkis);
+
+const random = [7, 777, 'labas', 9, true, 'rytas', false, []];
+
