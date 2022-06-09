d5a14ddf40a53d5b410171638419c0c96be1e4cd
Rafal K <Rafaledmund@yahoo.com>
2022-06-09 13:49:42 +0300

    namu darbai

--- js/nd.js
+++ js/nd.js
@@ -18,49 +18,45 @@ console.log(n3);
 
 //Sukurti 3 sąrašo tipo kintamuosius su penkiomis skaičių tipo reikšmėmis
 //Po kiekvieno jų inicijavimo, išvesti į console
-let d = [1, 2, 3, 4, 5];
-let e = [2, 3, 4, 5, 6];
-let f = [3, 4, 5, 6, 7];
-console.log(d);
-console.log(e);
-console.log(f);
+const s1 = [1, 2, 3, 4, 5];
+const s2 = [2, 3, 4, 5, 6];
+const s3 = [3, 4, 5, 6, 7];
+console.log(s1);
+console.log(s2);
+console.log(s3);
 
 //Sukurti 3 sąrašo tipo kintamuosius su penkiomis teksto tipo reikšmėmis
 //Po kiekvieno jų inicijavimo, išvesti į console
-const person = {
-    firstName : "John",
-    lastName  : "Doe",
-    age       : '50',
-    eyeColor  : "blue",
-    bodySize  : 'slim'
-  };
-const person2 = {
-    firstName : "Rambo",
-    lastName  : "Killer",
-    age       : '30',
-    eyeColor  : "Dark",
-    bodySize  : 'Fit'
-};
-const Person3 =  {
-    firstName : "Rambo",
-    lastName  : "Killer",
-    age       : '30',
-    eyeColor  : "Dark",
-    bodySize  : 'Fit'
-};
-console.log(person);
-console.log(person2);
-console.log(Person3);
+const t1 = ['Titanic', 'is', 'a', 'big', 'ship'];
+const t2 = ['The', 'Plane', 'is', 'a', 'bird'];
+const t3 = ['The', 'Glass', 'looks', 'like', 'cup'];
+
+console.log(t1);
+console.log(t2);
+console.log(t3);
+
 //Susumuoti visus skaičiaus tipo kintamuosius
 //Rezultatą išvesti į console
-console.log('Suma visu kintamuju yra', a+b+c+d+e+f);
+const suma = s1[0]+s1[1]+s1[2]+s1[3]+s1[4];
+console.log('Pirmo suma lygu ',suma);
+const suma1 = s2[0]+s1[1]+s1[2]+s1[3]+s1[4];
+console.log('Antro suma lygu ',suma1);
+const suma2 = s3[0]+s1[1]+s1[2]+s1[3]+s1[4];
+console.log('Trecio suma lygu ',suma2);
+
 
 //Sujungti visus teksto tipo kintamuosius taip, jog tarp jų būtų sudarytas tarpas
 //Rezultatą išvesti į console
-//colsole.log(person.firstName + " is " + person.age + " years old.");
+const text = t1[0] + ' ' + t1[1] + ' ' + t1[2] + ' ' + t1[3] + ' ' + t1[4]+ '. ' + t2[0] + ' ' + t2[1] + ' ' + t2[2] + ' ' + t2[3] + ' ' + t2[4] + '. ' + t3[0] + ' ' + t3[1] + ' ' + t3[2] + ' ' + t3[3] + ' ' + t3[4];
+console.log(text);
 
 //Apskaičiuoti vertę iš sąrašų kurių verčių tipas yra skaičiai, pagal pateiktą logiką
 //1-2+3-4+5
 //Rezultatą išvesti į console
+console.log('-----------------------------');
+const skaiciai = [1, 2, 3, 4, 5];
+const verte = skaiciai[0]-skaiciai[1]+skaiciai[2]-skaiciai[3]+skaiciai[4];
+console.log(verte);
 
-
+// Sujungti sąrašų vertes, kurių tipas yra tekstai, nuo sąrašo galo iki pradžios taip, 
+//jog tarp jų būtų kablelis ir tarpas
