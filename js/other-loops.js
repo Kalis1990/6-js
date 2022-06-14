19999825e5a7ee65727d1ea81544f225aa82d864
Rafal K <Rafaledmund@yahoo.com>
2022-06-14 11:31:56 +0300

    for,foroff,while,dowhilte,foreach,map,mapfilter,reduce

---
+++ js/other-loops.js
@@ -0,0 +1,89 @@
+const marks = [10, 2, 8, 4, 6];
+// for
+for (let i = 0; i < marks.length; i++){
+    const mark = marks[i];
+    console.log(mark);
+}
+
+//for-of
+for(const mark of marks){
+    console.log(mark);
+}
+console.log('-----------while----------');
+//while
+let i = 0;
+while (i < marks.length){
+    const mark = marks[i];
+    console.log(mark);
+    i++;
+}
+console.log('-----------do while----------');
+//do-while
+let i2 = 0;
+do {
+    const mark = marks[i2];
+    console.log(mark);
+    i2++;
+} while(i2 < marks.length)
+
+console.log('-----------foreach----------');
+//foreach
+marks.forEach(mark =>{
+    console.log(mark)
+});
+
+//map tik modifikuoja
+console.log('=========map===========')
+const rezMap = marks
+.map(mark => mark * 2)
+.map(mark => mark > 10 ? 10 :mark)
+
+const rezMap2 = marks
+.map(mark => mark * 2 > 10 ? 10 :mark * 2)
+
+console.log(marks);
+console.log(rezMap);
+console.log(rezMap2);
+
+//filtravimas
+console.log('--------filter');
+const filterMarks = marks
+.filter(mark => mark > 2)
+.filter(mark => mark < 7);
+
+const filterMarks2 = marks.filter(mark => mark >2 && mark < 7);
+console.log(marks)
+console.log(filterMarks)
+console.log(filterMarks2)
+
+//mark & filter combo
+console.log('-------filter------------');
+const mapFilter = marks
+.map(mark=> mark * 2)
+.filter(mark => mark >5)
+console.log(mapFilter)
+console.log(marks)
+
+// reduce
+console.log('-------reduce------------');
+const sumReduce = marks.reduce((total, mark) => total + mark);
+const sumReduce20 = marks.reduce((total, mark) => total + mark, 20);
+console.log(sumReduce)
+console.log(sumReduce20)
+
+const difReduce = marks.reduce((total, mark) => total - mark);
+const difReduce20 = marks.reduce((total, mark) => total - mark, 20);
+console.log(difReduce)
+console.log(difReduce20)
+
+const multiReduce = marks.reduce((total, mark) => total * mark);
+const multiReduce20 = marks.reduce((total, mark) => total * mark, 20);
+console.log(multiReduce)
+console.log(multiReduce20)
+
+const devReduce = marks.reduce((total, mark) => total / mark);
+const devReduce20 = marks.reduce((total, mark) => total / mark, 20);
+console.log(devReduce)
+console.log(devReduce20)
+
+
