f9b36a250b3d45062af7f3f92950ce2da08d50fa
Rafal K <Rafaledmund@yahoo.com>
2022-06-16 11:49:05 +0300

    truphy and falsy values

---
+++ js/ifValues.js
@@ -0,0 +1,25 @@
+//Reiksmiu pozityvumas /negativumas
+//skaiciai
+//0 - negatyvus
+//kiti - pozityvus
+//boolean:
+//True poz false ne pozityvus
+//string:
+//ne tuscias - pozityvus
+//tuscias - nepozityvus
+//array: 
+//visada nepozityvus
+//object: 
+//visada nepozityvus
+//funkcija: 
+//visada nepozityvus
+
+console.clear()
+
+
+const userInput = '';
+if(userInput){
+    console.log('taip');
+}else {
+    console.log('ne');
+}
