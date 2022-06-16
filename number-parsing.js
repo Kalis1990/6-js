1c95a609b2f3e558257c46aa2acce5883c9f963e
Rafal K <Rafaledmund@yahoo.com>
2022-06-16 11:37:01 +0300

    parsings

--- number-parsing.js
+++ number-parsing.js
@@ -1,10 +1,10 @@
-const n1 ='dsad7dsa';
+const n1 ='8';
 const n2 =5;
 
 function suma(a, b){
     const initialAsize = ('' + a).length;
     const initialbsize = ('' + b).length;
-    a=parseInt(a);
+    a=parseFloat(a);
     b=parseInt(b);
 
     const afterAsize = ('' + a).length;
@@ -25,26 +25,4 @@ function suma(a, b){
 const ats = suma(n1,n2);
 console.log(ats);
 
-
-
-console.log();
-
-const n3 ='7uhi';
-const n4 =5;
-
-function suma(c, d){
-    c=parseInt(c);
-    d=parseInt(d);
-        if(isNaN(c)){
-        return 'error: pirmas parametras ne skaicius'
-    }
-
-    if(typeof d!== 'number'){
-    
-        return 'error: antras parametras ne skaicius'
-    
-}
-    return c + d;
-}
-const odp = suma(n3,n4);
-console.log(odp);
+module.exports = suma;
---
+++ numberImport.js
@@ -0,0 +1,5 @@
+const suma = require('./js/number-parsing.js')
+
+const n1 ='8';
+const n2 =5;
+console.log(n1, n2);
